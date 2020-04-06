1183f2accb94946342753e293a77c0adda1766fd
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-04-06 13:29:12 -0400

    local-server.py: Fix URL handling (again)

--- local-server.py
+++ local-server.py
@@ -68,6 +68,10 @@ app = Flask(__name__)
 
 
 class LinkRewritingTreeProcessor(markdown.treeprocessors.Treeprocessor):
+    def __init__(self, md: markdown.Markdown, page: str) -> None:
+        super().__init__(md)
+        self.page = page.strip("/")
+
     def run(self, root: xml.etree.ElementTree.Element) -> None:
         self.handle_element(root)
 
@@ -93,7 +97,17 @@ class LinkRewritingTreeProcessor(markdown.treeprocessors.Treeprocessor):
                     if path.endswith(".md"):
                         path = path[:-3]
 
+                    # Add a trailing slash
                     path += "/"
+
+                    if not path.startswith("/"):
+                        # Make the path absolute by combining with the previous page
+                        if "/" in self.page:
+                            parent_page = self.page.rsplit("/", 1)[0]
+                        else:
+                            parent_page = ""
+
+                        path = os.path.join("/", parent_page, path)
 
                     # Recombine and use the new path
                     new_parts = (parts.scheme, parts.netloc, path, parts.query, parts.fragment)
@@ -103,8 +117,11 @@ class LinkRewritingTreeProcessor(markdown.treeprocessors.Treeprocessor):
 
 
 class LinkRewritingExtension(markdown.extensions.Extension):
+    def __init__(self, page: str) -> None:
+        self.page = page
+
     def extendMarkdown(self, md: markdown.Markdown) -> None:
-        md.treeprocessors.register(LinkRewritingTreeProcessor(md), "link_rewriter", -100)
+        md.treeprocessors.register(LinkRewritingTreeProcessor(md, self.page), "link_rewriter", -100)
 
 
 def load_doc_page(page: str):
@@ -131,11 +148,11 @@ def load_doc_page(page: str):
 
     # Render index.md within directories
     potential_paths = [
-        base_path + ".md",
-        os.path.join(base_path, "index.md"),
+        (base_path + ".md", ""),
+        (os.path.join(base_path, "index.md"), "index"),
     ]
 
-    for path in potential_paths:
+    for path, extra_part in potential_paths:
         # Check if the path exists first
         if not os.path.exists(path):
             continue
@@ -164,7 +181,10 @@ def load_doc_page(page: str):
                 "meta",
                 "nl2br",
                 markdown.extensions.toc.TocExtension(),
-                LinkRewritingExtension(),
+                # We add on the "extra part" because if this was a file like /a/index.md,
+                # then links should be interpreted relative to /a.
+                # If it was just /a.md, they should be interpreted relative to /.
+                LinkRewritingExtension((page + "/" + extra_part).rstrip("/")),
             ],
             tab_length=4,
             output_format="html5",
